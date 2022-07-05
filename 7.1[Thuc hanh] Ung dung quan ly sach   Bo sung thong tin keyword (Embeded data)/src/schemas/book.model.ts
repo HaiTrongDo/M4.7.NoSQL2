4088985a3d08645ce16d5bae71fee1dea7db6867
HaiTrongDo <dotronghai91@gmail.com>
2022-07-05 22:36:28 +0700

    ...

---
+++ 7.1[Thuc hanh] Ung dung quan ly sach   Bo sung thong tin keyword (Embeded data)/src/schemas/book.model.ts
@@ -0,0 +1,22 @@
+import {Schema, model} from "mongoose";
+
+interface IBook {
+    title: string;
+    description: string;
+    author: string;
+    keywords: object[];
+}
+
+const keywordsSchema = new Schema({
+    keyword: String
+})
+
+const bookSchema = new Schema<IBook>({
+    title: String,
+    description: String,
+    author: String,
+    keywords: [keywordsSchema],
+})
+
+const Book = model<IBook>('NewBook', bookSchema);
+export { Book };
